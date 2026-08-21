71ef5147a5d7ea3bb10f7415572858b6fa4b6bd7
Paul Junod <donujp@gmail.com>
2026-08-21 18:02:56 -0400

    fix(cluster): recover single-file snapshot WAL tails

---
+++ vendor/hiqlite-wal/src/lib.rs
@@ -0,0 +1,22 @@
+// Copyright 2026 Sebastian Dobe <sebastiandobe@mailbox.org>
+
+#![doc = include_str!("../README.md")]
+
+pub use crate::writer::LogSync;
+pub use log_store::{LogStore, LogStoreReader};
+pub use shutdown::ShutdownHandle;
+pub use writer::Action;
+
+pub mod error;
+mod lockfile;
+mod log_store;
+mod log_store_impl;
+mod metadata;
+mod reader;
+mod shutdown;
+mod utils;
+mod wal;
+#[cfg(feature = "migration")]
+pub mod writer;
+#[cfg(not(feature = "migration"))]
+mod writer;
